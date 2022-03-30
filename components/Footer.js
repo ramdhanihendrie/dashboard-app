30bbda24c36e4c61f92d979fb217b4b58124eb4f
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-03-31 00:03:18 +0700

    initialize tailwind and create Header & Footer

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='text-center	bg-sky-500 py-1 text-white'>
+      <p>Made with ♥ by Hendrie Ramdhani</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='h-14 bg-gradient-to-r from-sky-500 to-indigo-500 text-white	'>
+      <div className='flex container mx-auto py-2'>
+        <div className='font-bold text-2xl	'>
+          Dashboard
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
