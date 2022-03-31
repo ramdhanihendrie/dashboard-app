efedca35ba8f8a90f11e8046fd22ceb5071ef795
ramdhanihendrie <50083363+ramdhanihendrie@users.noreply.github.com>
2022-03-31 20:25:08 +0700

    index to arrow function and InitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
+import { data } from 'autoprefixer'
 import Head from 'next/head'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
-export default function Home() {
+const Index = ({ datas }) => {
   return (
     <div>
       <Head>
@@ -11,8 +12,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-
+        
       <Footer />
     </div>
-  )
+  )       
 }
+
+Index.getInitialProps = async () => {
+  const res = await fetch('http://localhost:3000/api/datas')
+  const {data} = await res.json()
+  return {datas : data}
+}
+
+export default Index;
